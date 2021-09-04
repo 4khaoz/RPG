88a8e9f3647fd7717cd7537d874cc4a379518c01
Thai T. Nguyen <thaitung.nguyen@yahoo.de>
2021-09-04 22:03:09 +0200

    Added Resource-GameplayAttributes to AttributeSetGeneral.

--- Source/RPG/Private/Gameplay/AbilitySystem/RPGAttributeSetGeneral.cpp
+++ Source/RPG/Private/Gameplay/AbilitySystem/RPGAttributeSetGeneral.cpp
@@ -23,4 +23,65 @@ void URPGAttributeSetGeneral::PostGameplayEffectExecute(const FGameplayEffectMod
 void URPGAttributeSetGeneral::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, MaxMana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, MaxStamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, Hunger, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, MaxHunger, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, Thirst, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(URPGAttributeSetGeneral, MaxThirst, COND_None, REPNOTIFY_Always);
+}
+
+void URPGAttributeSetGeneral::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, Health, OldHealth);
+}
+
+void URPGAttributeSetGeneral::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, MaxHealth, OldMaxHealth);
+}
+
+void URPGAttributeSetGeneral::OnRep_Mana(const FGameplayAttributeData& OldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, Mana, OldMana);
+}
+
+void URPGAttributeSetGeneral::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, MaxMana, OldMaxMana);
+}
+
+void URPGAttributeSetGeneral::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, Stamina, OldStamina);
+}
+
+void URPGAttributeSetGeneral::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, MaxStamina, OldMaxStamina);
+}
+
+void URPGAttributeSetGeneral::OnRep_Hunger(const FGameplayAttributeData& OldHunger)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, Hunger, OldHunger);
+}
+
+void URPGAttributeSetGeneral::OnRep_MaxHunger(const FGameplayAttributeData& OldMaxHunger)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, MaxHunger, OldMaxHunger);
+}
+
+void URPGAttributeSetGeneral::OnRep_Thirst(const FGameplayAttributeData& OldThirst)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, Thirst, OldThirst);
+}
+
+void URPGAttributeSetGeneral::OnRep_MaxThirst(const FGameplayAttributeData& OldMaxThirst)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(URPGAttributeSetGeneral, MaxThirst, OldMaxThirst);
 }
--- Source/RPG/Public/Gameplay/AbilitySystem/RPGAttributeSetGeneral.h
+++ Source/RPG/Public/Gameplay/AbilitySystem/RPGAttributeSetGeneral.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "RPGAttributeSetGeneral.generated.h"
 
 // Uses macros from AttributeSet.h
@@ -30,9 +31,77 @@ public:
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_Health)
 	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, Health)
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_MaxHealth)
 	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, MaxHealth)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, Mana)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, MaxMana)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_Stamina)
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, Stamina)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_MaxStamina)
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, MaxStamina)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_Hunger)
+	FGameplayAttributeData Hunger;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, Hunger)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_MaxHunger)
+	FGameplayAttributeData MaxHunger;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, MaxHunger)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_Thirst)
+	FGameplayAttributeData Thirst;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, Thirst)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attributes", ReplicatedUsing = OnRep_MaxThirst)
+	FGameplayAttributeData MaxThirst;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSetGeneral, MaxThirst)
+
+protected:
+	/**
+	OnRep functions for GameplayAttributes
+	*/
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+	UFUNCTION()
+	virtual void OnRep_Mana(const FGameplayAttributeData& OldMana);
+
+	UFUNCTION()
+	virtual void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana);
+
+	UFUNCTION()
+	virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
+
+	UFUNCTION()
+	virtual void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina);
+
+	UFUNCTION()
+	virtual void OnRep_Hunger(const FGameplayAttributeData& OldHunger);
+
+	UFUNCTION()
+	virtual void OnRep_MaxHunger(const FGameplayAttributeData& OldMaxHunger);
+
+	UFUNCTION()
+	virtual void OnRep_Thirst(const FGameplayAttributeData& OldThirst);
+
+	UFUNCTION()
+	virtual void OnRep_MaxThirst(const FGameplayAttributeData& OldMaxThirst);
 };
