6528e8661a3dfa8341a1d9c424233192181b4e93
Thai T. Nguyen <thaitung.nguyen@yahoo.de>
2021-09-07 21:01:20 +0200

    Created RPGPlayerHUDWidget which is going to display Attributesretrieved from RPGPlayerState. AttributeSets are owned by PlayerState for replication.

--- Source/RPG/Public/Gameplay/Player/RPGPlayerState.h
+++ Source/RPG/Public/Gameplay/Player/RPGPlayerState.h
@@ -4,14 +4,70 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
 #include "RPGPlayerState.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class RPG_API ARPGPlayerState : public APlayerState
+class RPG_API ARPGPlayerState : public APlayerState, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 	
+public:
+	ARPGPlayerState();
+
+	/**
+	Implement IAbilitySystemInterface
+	*/
+	class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	class URPGAttributeSetGeneral* GetAttributeSetGeneral() const;
+	class URPGAttributeSetAdvanced* GetAttributeSetAdvanced() const;
+
+	/**
+	Getters for Attributes
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetHealth() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxHealth() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMana() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxMana() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetStamina() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxStamina() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetHunger() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxHunger() const;
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetThirst() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	float GetMaxThirst() const;
+
+protected:
+	/**
+	Gameplay Ability System
+	*/
+	UPROPERTY()
+	class URPGAbilitySystemComponent* ASC;
+
+	UPROPERTY()
+	class URPGAttributeSetGeneral* AttrSetGeneral;
+
+	UPROPERTY()
+	class URPGAttributeSetAdvanced* AttrSetAdvanced;
 };
